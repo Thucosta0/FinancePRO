2a3c3e54f8c34d372cf6ba69ee202fb8652b0442
Thucosta <thucosta28@gmail.com>
2025-05-04 04:56:01 -0300

    Adicionada função test.js mais simples e atualizado package.json

---
+++ netlify/functions/test.js
@@ -0,0 +1,9 @@
+exports.handler = async function() {
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: "Função test.js funcionando!",
+      timestamp: new Date().toISOString()
+    })
+  };
+} 
